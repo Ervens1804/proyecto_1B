77aeaf00dcb431fc63be7f2dbdecd5c33e21ebbe
Ervens Jean-charles <jcervens98@gmail.com>
2024-04-26 11:03:18 -0400

    archivo organizado y una pagina subdivide con frame y frameset

---
+++ javascript/pepe.js
@@ -0,0 +1,34 @@
+
+//let f = new Date();
+function titulo(){
+document.body.innerHTML = "<h1>i am learning git an github 2 </br> </h1>"
+}
+
+
+//titulo();
+
+
+
+function cuerpo(){
+    document.body.innerHTML = "<h2> capitulo nuevo </br> </h2>"   
+}
+
+
+
+function texto1(){
+    titulo(); 
+    cuerpo();
+    document.body.innerHTML = "<p> en ese proyecto, estoy iniciando al profundo en git  and github donde voy aprender los basicos y los necesarios para cualquier proyecto </br> </p>" 
+
+}
+
+/*
+function general(){
+    titulo(); 
+    cuerpo();
+    texto1();
+}*/
+
+
+texto1();
+
